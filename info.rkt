fdef8cc2289833d12f3fc33d2f9b8f5528e4e14e
Scott Moore <scott@thinkmoore.net>
2017-02-15 16:07:13 -0500

    fix typo in info.rkt

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 
 (define shill-plugin-name 'db)
-(define shill-plugin-require 'db_api/dp_api)
+(define shill-plugin-require 'db_api/db_api)
 (define shill-plugin-ambient '(make-dbview))
 (define shill-plugin-interfaces '(dbview))
