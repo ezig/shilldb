58489c74f97e430a464c28ddfe2916504e626aa0
Ezra <ezrazigmond@gmail.com>
2017-11-03 16:30:58 -0400

    Added update and join implementation

--- shilldb.rkt
+++ shilldb.rkt
@@ -5,14 +5,18 @@
 (struct shill-view (view
                     [fetch #:mutable]
                     [where #:mutable]
-                    [select #:mutable]))
+                    [select #:mutable]
+                    [join #:mutable]
+                    [update #:mutable]))
 
 (define (build-view view)
   (define (fetch v pre) (fetch-impl (shill-view-view (pre v))))
   (define (where v w) (build-view (where-impl (shill-view-view v) w)))
   (define (select v c) (build-view (select-impl (shill-view-view v) c)))
+  (define (join v1 v2 jcond) (build-view (join-impl (shill-view-view v1) (shill-view-view v2) jcond)))
+  (define (update v query pre [where ""]) (update-impl (shill-view-view (pre v)) query where))
 
-  (shill-view view fetch where select))
+  (shill-view view fetch where select join update))
 
 (define (mutator-redirect-proc i v) v)
 
@@ -36,6 +40,8 @@
          (define fetch/c (make-fetch/c val ctc (assoc "fetch" full-details)))
          (define where/c (make-where/c (assoc "where" full-details) full-details))
          (define select/c (make-select/c (assoc "select" full-details) full-details))
+         (define join/c (make-join/c (assoc "join" full-details) full-details))
+         (define update/c (make-update/c val ctc (assoc "update" full-details)))
          (unless (contract-first-order-passes? ctc val)
            (raise-blame-error blame val '(expected "a view" given "~e") val))
          (impersonate-struct val
@@ -44,7 +50,11 @@
                              shill-view-where (redirect-proc where/c)
                              set-shill-view-where! mutator-redirect-proc
                              shill-view-select (redirect-proc select/c)
-                             set-shill-view-select! mutator-redirect-proc))))))
+                             set-shill-view-select! mutator-redirect-proc
+                             shill-view-join (redirect-proc join/c)
+                             set-shill-view-join! mutator-redirect-proc
+                             shill-view-update (redirect-proc update/c)
+                             set-shill-view-update! mutator-redirect-proc))))))
 
 (struct enhance-blame/c (ctc msg)
   #:property prop:contract
@@ -93,12 +103,39 @@
          [(= dl 3)
           (->* (shill-view? (and/c string? (third details))) #:pre (second details) (view-proxy full-details #f))])
   "select"))
-  
+
+(define (make-join/c details full-details)
+  (define dl (length details))
+  (enhance-blame/c
+   (cond [(= dl 2)
+          (->* (shill-view? shill-view? string?) #:pre (second details) (view-proxy full-details #f))]
+         [(= dl 3)
+          (->* (shill-view? shill-view? (and/c string? (third details))) #:pre (second details) (view-proxy full-details #f))])
+   "join"))
+
+(define (make-update/c view ctc details)
+  (define (update-pre/c pre)
+    (make-contract
+     #:projection
+     (λ (b)
+       (λ (u)
+         (λ (v q p [w ""])
+           (u view q (λ (v) (p (with-contract b #:result ctc (pre v)))) w))))))
+  (define dl (length details))
+  (enhance-blame/c
+   (cond [(= dl 2)
+          (->* (shill-view? string? procedure?) (string?) #:pre (second details) any)]
+         [else
+          (and/c (->* (shill-view? string? procedure?) (string?) #:pre (second details) any) (update-pre/c (third details)))])
+  "update"))
+
 (define (view/c
          #:fetch [f (list "fetch" #f)]
          #:where [w (list "where" #f)]
-         #:select [s (list "select" #f)])
-  (view-proxy (list f w s) #f))
+         #:select [s (list "select" #f)]
+         #:join [j (list "join" #f)]
+         #:update [u (list "update" #f)])
+  (view-proxy (list f w s j u) #f))
 
 (define (fetch view) ((shill-view-fetch view) view values))
 
@@ -106,12 +143,23 @@
 
 (define (select view c) ((shill-view-select view) view c))
 
+(define (join v1 v2 jcond) ((shill-view-join v1) v1 v2 jcond))
+
+(define (update v query [where ""]) ((shill-view-update v) v query values where))
+
 (define (open-view filename table)
   (build-view (make-view-impl filename table)))
 
 (module+ test
-  (define/contract v1 (view/c #:fetch (list "fetch" #t (λ (v) (where v "a < 10")))
-                              #:select (list "select" #t))
-                              (open-view "test.db" "test"))
+  (define/contract v1 (view/c #:fetch (list "fetch" #t)
+                              #:select (list "select" #t)
+                              #:update (list "update" #t)
+                              #:where (list "where" #t))
+    (open-view "test.db" "test"))
   (define/contract v2 (view/c #:fetch (list "fetch" #t (λ (v) (select v "b")))) v1)
-  (fetch v2))
+  (define/contract v3 (view/c #:fetch (list "fetch" #t) #:join (list "join" #t)) (open-view "test.db" "test"))
+  (define/contract (f w v) (->i ([w integer?] [v (w) (view/c #:update (list "update" #t (λ (v) (where v (format "a = ~a" w)))))]) [result any/c])
+    (update v "b = b + 1"))
+  (fetch v1)
+  (f 5 v1)
+  (fetch v1))
