e55b8c1d8a2e4782f8712618bb61c7f5cd9d3df4
Ezra <ezrazigmond@gmail.com>
2017-12-26 15:13:40 -0500

    Working on modifier syntax

--- racket/private/shilldb-macros.rkt
+++ racket/private/shilldb-macros.rkt
@@ -10,39 +10,68 @@ suggested form for view/c
 |#
 
 (require (for-syntax syntax/parse)
+         (for-syntax (only-in "api/util.rkt"
+                              list-unique?))
          (except-in "shilldb.rkt" view/c))
 
 
 (begin-for-syntax
- 
- (define-syntax-class privilege-name
-   #:description "primitive privilege"
-   (pattern #:fetch)
-   (pattern #:update))
-
-
- (define-syntax-class privilege
-   #:description "privilege"
-   (pattern name:privilege-name
-            #:with (modifier ...) '())
-   (pattern [name:privilege-name modifier ...])))
-
-
-(define-syntax (privilege-parse stx)
-  (syntax-parse stx
-    [(_ p:privilege)
-     #`(list #,(keyword->string (syntax->datum #`p.name)) #t p.modifier ...)]))
+  (define valid-modifiers
+    (hash "fetch" (list '(#:restrict 0))
+          "update" (list '(#:restrict 0))))
   
-
-(define-syntax (view/c stx)
-  (syntax-parse stx
-    [(_ p:privilege ...)
-     #'(view-proxy (list (privilege-parse p) ...) #f)]))
-
-  (view/c [#:fetch #t #t] #:update)
-
-
-            
-
-
+  (define-syntax-class privilege-name
+    #:description "primitive privilege"
+    (pattern (~literal +fetch))
+    (pattern (~literal +update))
+    (pattern (~literal +delete))
+    (pattern (~literal +insert))
+    (pattern (~literal +where))
+    (pattern (~literal +select))
+    (pattern (~literal +join)))
+  
+  (define (privilege-stx->string name)
+    (substring (symbol->string (syntax->datum name)) 1))
+  
+  (define-syntax-class privilege-modifier
+    #:description "privilege modifier"
+    (pattern (name:keyword val:expr)))
+  
+  (define (validate-modifiers priv-name ks vs)
+    (let ([k-datums (map syntax->datum ks)]
+          [valid-mods (map car (hash-ref valid-modifiers priv-name))])
+      ;TODO: improve error messages
+      (unless (list-unique? k-datums)
+        (raise-syntax-error 'view/c "duplicate modifier for privilege"))
+      (for-each (λ (k) (unless (member k valid-mods)
+                         (raise-syntax-error 'view/c "invalid modifier for privilege"))) k-datums)
+      vs))
+    
+    (define-syntax-class privilege
+      #:description "privilege"
+      (pattern name:privilege-name
+               #:with ((~seq mod-name:keyword mod-val:expr) ...) '()
+               #:attr [proxy-args 1] '())
+      (pattern [name:privilege-name (~seq mod-name:keyword mod-val:expr) ...]
+               #:attr [proxy-args 1]
+               ;(println (syntax->list #'(mod-val ...))))))
+               (validate-modifiers (privilege-stx->string #'name) (syntax->list #'(mod-name ...)) (syntax->list #'(mod-val ...))))))
+  
+  (define-syntax (privilege-parse stx)
+    (syntax-parse stx
+      [(_ p:privilege)
+       #`(list #,(privilege-stx->string #`p.name) #t p.proxy-args ...)]))
+  
+  (define-syntax (view/c stx)
+    (syntax-parse stx
+      [(_ p:privilege ...)
+       #'(view-proxy (list (privilege-parse p) ...) #f)]))
+  
+  
+;(define/contract v
+;  (view/c [+fetch #:restrict (λ (v) (where v "grade < 90"))])
+;  (open-view "test.db" "students"))
+;(fetch v)
+           
+(view/c [+fetch #:a (λ (v) v)])
   
