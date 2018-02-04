e6b46f54623df06b7b7424ef0c6a1a155014af08
Ezra <ezrazigmond@gmail.com>
2018-02-04 18:54:31 -0500

    Refactor library server to make it easy to swap implementations

--- experiments/library/library_server.rkt
+++ experiments/library/library_server.rkt
@@ -3,9 +3,11 @@
 (require (planet dmac/spin))
 (require db)
 (require json)
+(require (prefix-in sdb: shilldb/private/out))
 (require (for-syntax racket/syntax))
 
 (define DBPATH "/Users/ezra/Dropbox/Shill/db_api/experiments/library/test.db")
+(define DBIMPL 'db)
 
 (define-syntax (with-db stx)
   (syntax-case stx ()
@@ -26,52 +28,69 @@
       (λ () (write-json (vector->list row)))))
 
 
+(define (execute-reserve user book)
+  (case DBIMPL
+    ['db (with-db
+            (query-exec db "insert into reservations (cardholder_id, book) values ($1, $2)" user book))]))
+
+(define (execute-my-reservations user)
+  (case DBIMPL
+    ['db (with-db
+             (rows-to-json (query-rows db
+                                       (string-append "select r_id, title, firstname, lastname "
+                                                      "from reservations, books, authors "
+                                                      "where book = book_id and author = author_id and cardholder_id = $1")
+                                       user)))]))
+
+(define (execute-remove-reservation user rid)
+  (case DBIMPL
+    ['db (with-db
+               (query-exec db "delete from reservations where r_id = $1" rid))]))
+
+(define (execute-search-author fname lname)
+  (case DBIMPL
+    ['db (with-db
+             (rows-to-json (query-rows db
+                                       (string-append "select book_id, title "
+                                                      "from books, authors "
+                                                      "where firstname = $1 and lastname = $2 and author = author_id")
+                                                      fname lname)))]))
+
+(define (execute-num-reservations book)
+  (case DBIMPL
+    ['db (with-db
+             (row-to-json (query-row db "select count(*) from reservations where book = $1" book)))]))
+
 (get "/reserve"
      (lambda (req)
        (begin
          (let ([user (params req 'user)]
                [book (params req 'book)])
-           (with-db
-               (query-exec db "insert into reservations (cardholder_id, book) values ($1, $2)" user book)))
-         "OK")))
+           (execute-reserve user book)))
+         "OK"))
 
 (get "/my_reservations"
      (lambda (req)
        (let ([user (params req 'user)])
-         (with-db
-             (rows-to-json (query-rows db
-                                       (string-append "select r_id, title, firstname, lastname "
-                                                      "from reservations, books, authors "
-                                                      "where book = book_id and author = author_id and cardholder_id = $1")
-                                       user))))))
+         (execute-my-reservations user))))
 
 (get "/remove_reservation"
      (lambda (req)
        (begin
          (let ([user (params req 'user)]
                [rid (params req 'rid)])
-           (with-db
-               (query-exec db "delete from reservations where r_id = $1" rid)))
+           (execute-remove-reservation user rid))
          "OK")))
 
 (get "/search_author"
      (lambda (req)
        (let ([fname (params req 'fname)]
              [lname (params req 'lname)])
-         (with-db
-             (rows-to-json (query-rows db
-                                       (string-append "select book_id, title "
-                                                      "from books, authors "
-                                                      "where firstname = $1 and lastname = $2 and author = author_id"
-                                                      fname lname)))))))
-
-(get "/search_book"
-     (lambda (req) "Hello!"))
+         (execute-search-author fname lname))))
 
 (get "/num_reservations"
      (lambda (req)
        (let ([book (params req 'book)])
-         (with-db
-             (row-to-json (query-row db "select count(*) from reservations where book = $1" book))))))
+         (execute-num-reservations book))))
 
 (run)
