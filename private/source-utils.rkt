a5e4809f84da91ff7d360909f29a303fdc2df25a
Ezra <ezrazigmond@gmail.com>
2018-03-06 23:07:18 -0500

    Temporarily disable bugged module checking

--- private/source-utils.rkt
+++ private/source-utils.rkt
@@ -23,7 +23,7 @@
                (let* ([src-mod (import-src-mod-path id)]
                       [src-mod-path (resolve-module-path (cond [(module-path? src-mod) src-mod]
                                                                [else (syntax->datum src-mod)])
-                                                         #f #;(syntax-source #'spec))]
+                                                         (syntax-source #'spec))]
                       [_ (dynamic-require src-mod-path (void))]
                       [mod-info 
                        (module->language-info src-mod-path)])
@@ -31,10 +31,10 @@
                         seen]
                        [(equal? mod-info '#(shilldb/cap/lang/language-info get-language-info #f))
                         (cons src-mod-path seen)]
-                       [else (error 
-                              'non-shilldb-module-required 
-                              "~a not a shilldb/cap module"
-                              src-mod-path)]))) 
+                       [else (cons src-mod-path seen)]))) ;(error 
+                              ;'non-shilldb-module-required 
+                              ;"~a not a shilldb/cap module"
+                              ;src-mod-path)]))) 
              imports)
            sources))]))))
 
